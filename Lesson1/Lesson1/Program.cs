f065bad0c171e2d35f76fa656555ac0ffc61b5b7
Виктория Шумская <overveg@gmail.com>
2021-04-20 12:54:24 +0300

    first task completed

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -6,7 +6,31 @@ namespace Lesson1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); 
+            do
+            {
+                Console.WriteLine("Введите целое число:");
+                string getNumber = Console.ReadLine();
+                bool success = Int32.TryParse(getNumber, out int number);
+                if (success)
+                {
+                    Console.WriteLine(isSimple(number));
+                }
+                Console.WriteLine();
+            } while (true);
+
+        }
+
+        static string isSimple(int n)
+        {
+            int d = 0;
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    d++;
+                }
+            }
+            return d == 0 ? "простое" : "не простое";
         }
     }
 }
